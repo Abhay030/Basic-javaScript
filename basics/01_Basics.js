1c8b4e2fc0bf48236034379334daced5afdeedb9
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2024-02-10 21:29:08 +0000

    second commit

--- basics/01_Basics.js
+++ basics/01_Basics.js
@@ -1,1 +1,11 @@
-console.log("Abhay");
+const name = "Abhay"  //values of the const cannot be changed after it is once initialised 
+var accountId = "2022.csai-----"  
+let pass = 12234
+city  = "kanpur"
+
+var accountId = "abhistar@gmail.com"  //updating the var after the first declaration
+
+// Prefer not to use the var type declarations because it have block scope and functional scope issues 
+
+console.log("name")
+console.table([name ,accountId , pass , city]);
