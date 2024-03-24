ec1cc583d37973814b23a43cb05a40675c0911b1
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2024-03-24 10:24:45 +0000

    Changes for a and b

--- basics/01_Basics.js
+++ basics/01_Basics.js
@@ -1,11 +1,12 @@
-const name = "Abhay"  //values of the const cannot be changed after it is once initialised 
-var accountId = "2022.csai-----"  
-let pass = 12234
-city  = "kanpur"
+const name = "Abhay"; //values of the const cannot be changed after it is once initialised
+var accountId = "2022.csai-----";
+let pass = 12234;
+city = "kanpur";
+// nam = "vinod"
 
-var accountId = "abhistar@gmail.com"  //updating the var after the first declaration
+var accountId = "abhistar@gmail.com"; //updating the var after the first declaration
 
-// Prefer not to use the var type declarations because it have block scope and functional scope issues 
+// Prefer not to use the var type declarations because it have block scope and functional scope issues
 
-console.log("name")
-console.table([name ,accountId , pass , city]);
+console.log("name"); // USWD FO THE SINGLE PRINTING OF THE VARIABLE
+console.table([name, accountId, pass, city]); //USWD FO THE MULTIPLE PRINTING OF THE VARIABLE
---
+++ basics/Datatypes.js
@@ -0,0 +1,15 @@
+//number => 2 to the power 53
+// bigint 
+// String => ""
+// boolen => true /false
+// null => it is the stand alone value
+// undefined => vairbale declared hai aur value initialized nhi hai
+// Symbol => unique
+
+
+
+//object
+let a = "Abhay"
+console.log(typeof(a)) // string
+console.log(typeof(null)) //Object
+console.log(typeof(undefined)) //undefined
