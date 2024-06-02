85b6cdf6dadcd3dd81990f725c430c22d8106961
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2024-06-02 18:29:22 +0000

    stack and heap

---
+++ basics/05_Stack_and_heap.js
@@ -0,0 +1,29 @@
+// Stack(use hota hai PRIMITIVE ke liye) , heap(use hota hai NONO-PRIMITIVE ke liye)
+
+// primitive mtlb value change karne pe main object ki value change nhi hogi 
+// non-primitive mtlb values change karne pe change ho jaegi main wale ki
+
+
+
+
+// //PRIMITIVE
+// let my_name = "Abhay";
+// let anothername = my_name
+
+// anothername = "Abhi"
+// console.log(my_name);
+// console.log(anothername);
+
+
+
+//NON-PRIMITIVE
+// let user1 ={
+//     name: "Abhay",
+//     age : 21
+// }
+
+// let user2 = user1
+// user2.name = "abhi"
+// console.log(user1);
+// console.log(user2);
+
