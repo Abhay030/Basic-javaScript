fb63daf8c54a8c23028e715252a6024f85b062af
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2024-06-02 14:14:07 +0000

    Conversion of DataTypes

--- basics/03_conversion.js
+++ basics/03_conversion.js
@@ -1,28 +1,38 @@
-//NUMBER TO BOOLEAN CONVERSION
-let loggedin = 1
-console.log(loggedin); //1
-let IsloggesIn = Boolean(loggedin)
-console.log(IsloggesIn); //true
-// 1 => true , 0 => false
-// "" => false
-// "abhay" = >true
+// //NUMBER TO BOOLEAN CONVERSION
+// let loggedin = 1
+// console.log(loggedin); //1
+// let IsloggesIn = Boolean(loggedin)
+// console.log(IsloggesIn); //true
+// // 1 => true , 0 => false
+// // "" => false
+// // "abhay" = >true
 
-//STRING TO NUMBER CONVERSION
-let a = "35is"
-console.log(a);  //returns 35is
-let b = Number(a)
-console.log(b); // returns NaN
+// //STRING TO NUMBER CONVERSION
+// let a = "35is"
+// console.log(a);  //returns 35is
+// let b = Number(a)
+// console.log(b); // returns NaN
 
-let w = "35"
-let r = "78"
-console.log(w+r); // STRING CONCATENATION
+// let w = "35"
+// let r = "78"
+// console.log(w+r); // STRING CONCATENATION
 
-let t = Number(w) + Number(r) //ADDITION by conversion of the string to the number 
-console.log(t);
+// let t = Number(w) + Number(r) //ADDITION by conversion of the string to the number 
+// console.log(t);
 
 
 //NUMBER TO STRING CONVERSION
 let q = 98
 let c = String(q)
 console.log(q);
-console.log(typeof q);
+console.log(typeof q);  // number
+console.log(typeof c);  // string
+
+
+// // any string is true if we convert it into booolena type
+// // but an empty string is false , if we just put a space in a string the it will also be a true 
+// let a  = "false" 
+// let b = ""
+// console.log(Boolean(a) , Boolean(b));
+
+
