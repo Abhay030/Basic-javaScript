0c560bfbb698f4cae08a05a31ea27b29a1bc8b97
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2024-06-02 15:21:53 +0000

    leearned about primitive and non primitive DT and there usage

---
+++ basics/02_Datatypes.js
@@ -0,0 +1,64 @@
+// Primitive data TYpes
+
+// mtlb inki values change nhi hogi only reference variables ki values chamge hogi.
+//  7 types :  String , Number , Boolean , null , undefined , Symbol, bigInt.
+
+
+// these Symbols are mostly used in the react
+
+// const id = Symbol('123')
+// const anotherid = Symbol('123')
+// console.log(id === anotherid);  // false
+// console.log(id == anotherid);   //false
+// console.log(typeof(id));
+
+// const bigint = 345678900879876567654
+// console.log(bigint);
+
+
+
+
+
+// JAVACRIPT IS DYNAMICALLY OR STATICALLY TYPED ?
+
+// Dynamically-typed languages are those (like JavaScript) where the interpreter assigns variables a type at runtime based on the variable's value at the time.
+
+
+
+
+
+
+
+
+// Reference type or (Non Primitive) 
+// Arrays , Objects , Functions
+
+
+// //ARRAY
+// const heros = ["iron man" , " thor" , " loki"]
+// heros.forEach(element => {
+//     console.log(element + " mai hu");
+// });
+// console.log(heros);
+
+
+//OBJECTS
+let MyObj = {
+    name : "Abhay",
+    78 : 56,
+    68 : 56,
+    58 : 56 ,
+    name2 : "vaibhav"
+};
+// Integer keys (non-negative integers) are listed in ascending order.
+// String keys and symbol keys follow in the order they were added to the object.
+console.log(MyObj);
+
+
+const myfunction = function () {
+    console.log("Hello World");
+}
+
+
+
+
---
+++ basics/04_comparison.js
@@ -0,0 +1,21 @@
+// // here the javaScript converts the data type of the first boolean vallue to the number .
+// console.log(true > 1);
+// console.log(true == 1);
+// console.log(true < 1);
+// console.log(true < 2);
+// console.log(2  > false);
+
+// console.log(null > 0);  // false
+// console.log(null == 0);  // false
+
+// // <= and >= works differently so the null is converted into the 0 and in the above two cases the null is treated as null only 
+// console.log(null <= 0);  // true
+
+
+
+// === 
+// console.log("2" === 2); // this will give false as it compares te data type also.
+
+
+//////////////////    THE ABOVE TYPE OF THE COMPARISONS ARE NOT OKKAY TO BE USED DURING ANY PROJECTS AS IT LEAVES INCONSISTENCY ///////////////////// 
+
