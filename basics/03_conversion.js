3e8a0b2ec78d6b500bbe808d60d4be2c227c6d48
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2024-06-02 14:27:52 +0000

    Operators

--- basics/03_conversion.js
+++ basics/03_conversion.js
@@ -21,18 +21,30 @@
 // console.log(t);
 
 
-//NUMBER TO STRING CONVERSION
-let q = 98
-let c = String(q)
-console.log(q);
-console.log(typeof q);  // number
-console.log(typeof c);  // string
+// //NUMBER TO STRING CONVERSION
+// let q = 98
+// let c = String(q)
+// console.log(q);
+// console.log(typeof q);  // number
+// console.log(typeof c);  // string
 
 
 // // any string is true if we convert it into booolena type
 // // but an empty string is false , if we just put a space in a string the it will also be a true 
-// let a  = "false" 
-// let b = ""
-// console.log(Boolean(a) , Boolean(b));
+// let a1  = "false" 
+// let b1 = ""
+// console.log(Boolean(a1) , Boolean(b1));
+
+
+
+
+// ****************  OPERATORS ******************
+
+// let gamecounter  = 100;
+// console.log(gamecounter++); // first use then increment
+// console.log(gamecounter);  
+// console.log(++gamecounter); //first increment then use
+// console.log(gamecounter);
+
 
 
