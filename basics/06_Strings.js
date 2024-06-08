0148839d27f6b2cf6f7c2c42afcb245c836c6f94
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2024-06-08 14:28:39 +0000

    Learned Maths and Strings

---
+++ basics/06_Strings.js
@@ -0,0 +1,48 @@
+// //old way of writing the string concatenation 
+// const name  = "abhay"
+// const repocount = 50
+// console.log(name +" " + repocount);
+
+
+
+// // Modern style of writing using formatting
+// console.log(`hello my name is ${name} and my repro count is ${repocount}`);
+
+
+
+// // Declaration using new keyword
+// const gameName = new String('Abhay')
+// console.log(gameName.__proto__);
+// console.log(gameName.charAt(0));
+// console.log(gameName.toUpperCase());
+// console.log(gameName.length);
+// console.log(gameName.endsWith('y'));
+// console.log(gameName.substring(0,6));
+
+
+
+// // IT ALSO ACCEPTS NEGATIVE VALUES
+// console.log(gameName.slice(-1,5));
+
+
+
+// //IT TRIMS THE EXTRA SPACES IN THE STRING
+// const newString = "   bhaiya"
+// console.log(newString.trim());
+
+
+
+// //REPLACE THE STRINGS
+// const url = "https://www.youtube.com/watch?v=sscX432bMZo&t=8648s"
+// console.log(url.replace("youtube","Abhay"))
+
+
+// // FORMS AN ARRAY SPLITTED BY THE SPILLTER VALUE GIVEN IN THE METHOD 
+// const games = "gta-pubg-bgmi-coc"
+// console.log(games.split('-')); 
+
+
+
+
+
+
---
+++ basics/07_nums_and_maths.js
@@ -0,0 +1,38 @@
+// const score = 400
+// console.log(score); // 400
+
+// const game = new Number(400)
+// console.log(game);   // [Number: 400]
+
+// // WE CAN USE THE METHODS DEFINED FOR THE NUMBER TYPE BUT THE (GAME) IS OF OBJECT TYPE AND (SCORE) IS OF NUMBER TYPE
+// console.log(game.toString());
+// console.log(score.toString());
+// console.log(typeof(game) +" "+ typeof(score)); // object number
+
+
+//SOMEUSEFUL PROPERTIES
+// console.log(score.toFixed(3)); //400.000
+// const otherNumber = 1233.6789
+// // console.log(otherNumber.toPrecision(5)); // 1233.7
+// const hundreds = 100000000000
+// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(Number.MIN_VALUE);
+// console.log(Number.isSafeInteger());
+
+
+
+// ************************MATHS*********************
+
+// //SOME USEFUL FUNCTION
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.53));
+// console.log(Math.SQRT1_2)
+// console.log(Math.cos(30));
+// console.log(Math.random());
+// console.log(Math.max(4,3,6,8));
+// console.log(Math.min(4,3.1,6,8));
+
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
